f8f94d47258e006b376dfe54669e9eb1da75db35
umiiii419 <umiiii419@gmail.com>
2021-07-31 11:13:16 +0000

    [Add]createメソッドを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,10 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book.id)
   end
 
   def index
