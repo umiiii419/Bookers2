47f2d528cb5d86a0ea9c52bceaf3043d0a7c6372
umiiii419 <umiiii419@gmail.com>
2021-08-03 06:24:55 +0000

    [Add]showアクションに@booksを渡す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,11 @@ class UsersController < ApplicationController
   
   def show
     @user = @User.find(params[:id])
+    @books = @user.books.page(params[:page]).reserve_order
   end
   
   def index
-    @user = User
+    @user = User.all
   end
   
 end
