0d61002b2aea4674756a6d9fd27cf1df355cb098
umiiii419 <umiiii419@gmail.com>
2021-08-03 06:26:54 +0000

    [Add]editアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
   
   def show
-    @user = @User.find(params[:id])
+    @user = User.find(params[:id])
     @books = @user.books.page(params[:page]).reserve_order
   end
   
   def index
     @user = User.all
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   
 end
