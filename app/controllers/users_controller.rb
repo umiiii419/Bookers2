fb837119a7e39b501700d2357c68017ee15be5c2
umiiii419 <umiiii419@gmail.com>
2021-08-03 23:52:58 +0000

    [Add]updateアクションとStrong Parametersを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,29 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books.page(params[:page]).reserve_order
   end
-  
+
   def index
     @user = User.all
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
-  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
